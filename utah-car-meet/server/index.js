34317e5fdd549ec95e84360338d8dd29988cec54
Tayte Stokes <taytestokes9328@gmail.com>
2018-05-22 10:12:19 -0600

    Setup node server

---
+++ utah-car-meet/server/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const {
+    PORT
+} = process.env;
+
+app.use(bodyParser.json());
+
+
+app.listen(PORT, console.log(`Firing on ${PORT}`))
