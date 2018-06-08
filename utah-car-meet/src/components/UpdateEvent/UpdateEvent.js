2476113b899d2380ec19a86473c20c9b845ba5c9
Tayte Stokes <taytestokes9328@gmail.com>
2018-06-08 10:17:38 -0600

    Push before I try to apply socket.io

--- utah-car-meet/src/components/UpdateEvent/UpdateEvent.js
+++ utah-car-meet/src/components/UpdateEvent/UpdateEvent.js
@@ -5,7 +5,12 @@ import {connect} from 'react-redux';
 //Reducer Functions
 import {updateTitle, updateLocation, updateDate, updatePicture, updateEventInfo, resetState} from '../../ducks/events';
 
+//Material-UI Componenets
 import Navbar from '../NavBar/Navbar';
+
+//StyleSheets
+import './reset.css';
+import './UpdateEvent.css';
 
 class UpdateEvent extends Component {
     updateEvent(){
@@ -13,34 +18,41 @@ class UpdateEvent extends Component {
         let {title, location, date, event_picture, event_info} = this.props;
         axios.put(`/api/event/${id}`, {title, location, date, event_picture, event_info, id}).then(() => {
             this.props.history.push(`/event/page/${id}`)
-        })
+        }).catch(error => console.log(error))
     }
 
     render(){
         return(
-            <div>
+            <div className="update-event-container">
                 <Navbar />
-                Update page
+                    <div className="update-event-center-container">
+                        
+                        <div className="update-event-title-container">
+                            Title <input type="text" onChange={(event) => {
+                                this.props.updateTitle(event.target.value);
+                            }}/>
+                        </div>
 
-                Title <input type="text" onChange={(event) => {
-                    this.props.updateTitle(event.target.value);
-                }}/>
+                        <div className="update-event-location-container">
+                            Location <input type="text" onChange={(event) => {
+                                this.props.updateLocation(event.target.value);
+                            }}/>
+                        </div>
 
-                Location <input type="text" onChange={(event) => {
-                    this.props.updateLocation(event.target.value);
-                }}/>
-
-                Date <input type="date" onChange={(event) => {
-                    this.props.updateDate(event.target.value);
-                }}/>
-
-                Event Picture <input type="file" onChange={(event) => {
-                    this.props.updatePicture(event.target.value);
-                }}/>
-
-                Event Info <input type="text" onChange={(event) => this.props.updateEventInfo(event.target.value)} />
-
-                <button onClick={() => this.updateEvent()}>Submit Update</button>
+                        <div className="update-event-date-container">
+                            Date <input type="date" onChange={(event) => {
+                                this.props.updateDate(event.target.value);
+                            }}/>
+                        </div>
+                        
+                        <div className="update-event-info-container">
+                            Event Info <input type="text" onChange={(event) => this.props.updateEventInfo(event.target.value)} />
+                        </div>
+                        
+                        <div className="update-event-btn-container">
+                            <button onClick={() => this.updateEvent()}>Submit Update</button>
+                        </div>
+                    </div>
             </div>
         )
     }
